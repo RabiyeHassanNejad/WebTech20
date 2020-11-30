e84534c4b008b7e1b4a1f18f01258432c4159657
RabiyeHassanNejad <Rabiye.HassanNejad@Student.HTW-Berlin.de>
2020-11-30 15:56:42 +0100

    Abgabe 7

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,14 @@
+import { NgModule } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
+import {TableComponent} from './table/table.component';
+
+const routes: Routes = [
+  { path: 'table', component: TableComponent },
+  { path: 'table/:id', component: SingleComponent }
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
