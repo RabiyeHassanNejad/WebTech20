c1c99f0e418e08597380b9dcef7b5c1623086bb3
RabiyeHassanNejad <Rabiye.HassanNejad@Student.HTW-Berlin.de>
2020-12-14 17:34:58 +0100

    Uebungaufgabe 9

---
+++ frontend3/src/app/app.module.ts
@@ -0,0 +1,22 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { FormComponent } from './members/read/form/form.component';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    FormComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    NgbModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
